89718452bda38cdc82895b39bc3734163ac21d9f
Luiza Alexandru <alexandru97luiza@yahoo.com>
2024-05-23 19:06:03 +0300

    Problema 2

--- Alexandru_Luiza.py
+++ Alexandru_Luiza.py
@@ -5,3 +5,10 @@ def loadTxt(caletxt):
     with open(caletxt,'r') as file:
         fisier=file.readlines()
         return fisier
+    
+# 3.2 scrieti o functie generateListFromString() care primeste ca parametru un string si returneaza o lista pe baza acestuia. stringul va avea formatul (string,string,string)
+# dati commit la schimbari in repositoryul global
+def generateListFromString(string,separator):
+    lista=string.split(separator)
+    return lista
+
