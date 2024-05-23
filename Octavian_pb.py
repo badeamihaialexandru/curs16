23ea75807bb5415c5b0ec722a0a24a53fe9872be
taviiii27 <petrucretu03@gmail.com>
2024-05-23 19:08:56 +0300

    Add files via upload

---
+++ Octavian_pb.py
@@ -0,0 +1,15 @@
+def loadTxt(filename):
+    with open(filename, 'r') as file:
+        content = file.read()
+    return content
+
+def generateListFromString(string):
+    return string.split(',')
+
+def uniques(input_list):
+    return list(set(input_list))
+
+def writeListToFile(output_list, filepath):
+    with open(filepath, 'w') as file:
+        for item in output_list:
+            file.write(f"{item}\n")
