21e5018d6d70947cde9930cde8c9c04011422177
aluVoda <florentin.voda@gmail.com>
2024-05-23 19:44:16 +0300

    3.2

--- VodaFlorentin.py
+++ VodaFlorentin.py
@@ -1,8 +1,15 @@
 #3.1
 
-
 def loadtxt(txt):
    with open (txt, 'r')as file:
       list=file.read
       return list
+
+#3.2
+
+def generateListFromString(string):
+   list=[]
+   for i in string:
+      list.append(i)
+   return list
 
