d086ba31ab580a22c8a80c5fa83cdb3bddaabcce
Slatinat Dean <deanslatinat@yahoo.com>
2024-05-23 19:09:23 +0300

    Dean

--- SlatinatDean.py
+++ SlatinatDean.py
@@ -2,4 +2,11 @@ def loadTxt(path):
     with open(path,'r') as file:
         content=file.read()
         return content
-print(loadTxt('Curs13/input.txt'))
+# print(loadTxt('Curs13/input.txt'))
+
+def generateListFromString(string):
+    lista=[]
+    for nr in range(3):
+        lista.append(string)
+    return lista
+print(generateListFromString('Macarena'))
