219c963fc37aeb90b5bba4cf7cc98a19d34f4180
taviiii27 <petrucretu03@gmail.com>
2024-05-23 19:36:37 +0300

    Add files via upload

--- Octavian_pb.py
+++ Octavian_pb.py
@@ -9,7 +9,7 @@ def generateListFromString(string):
 def uniques(input_list):
     return list(set(input_list))
 
-def list_to_txt(output_list, filepath):
+def writeListToFile(output_list, filepath):
     with open(filepath, 'w') as file:
         for item in output_list:
             file.write(f"{item}\n")
