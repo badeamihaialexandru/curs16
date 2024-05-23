230e1559ab6ee37fa102e818c85d7819960bdd15
unknown <mihaitg19@gmail.com>
2024-05-23 19:33:56 +0300

    tMihaiproblema3.4

--- TeglerMihai.py
+++ TeglerMihai.py
@@ -22,3 +22,12 @@ def uniques(y):
 
 list=[1,2,3,1,2,6,1,10,0,2,3,1,1,2,15,201,10,20,3]
 print(uniques(list))
+
+
+def listToTxt(y):
+    with open('lista.txt','w') as file:
+        unique_set=uniques(y)
+        for item in unique_set:
+            file.write(f'{item}\n')
+
+listToTxt(list)
