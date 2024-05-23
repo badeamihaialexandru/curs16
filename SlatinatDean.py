7c5dbc37d91b9c9b349e27f8c8b00513cfcff753
Slatinat Dean <deanslatinat@yahoo.com>
2024-05-23 19:30:27 +0300

    done

--- SlatinatDean.py
+++ SlatinatDean.py
@@ -2,23 +2,22 @@ def loadTxt(path):
     with open(path,'r') as file:
         content=file.read()
         return content
-# print(loadTxt('Curs13/input.txt'))
+
 
 def generateListFromString(string):
     lista=[]
     for nr in range(3):
         lista.append(string)
     return lista
-# print(generateListFromString('Macarena'))
+
 
 def uniques():
     lista=['Dean','Dean','Andrei','Andrei']
     lista=set(lista)
     return lista
-# print(uniques())
 
-def listaInTxt():
+
+def listaToTxt():
     listaUnica=uniques()
     with open('Curs16/curs16/uniques.txt','w') as file:
         file.write(str(listaUnica))
-listaInTxt()
