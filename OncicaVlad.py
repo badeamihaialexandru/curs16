2e08e78c3a64e09f136a32b73d4414d5748333e8
Alshmenu <vlad.oncica@gmail.com>
2024-05-23 19:30:04 +0300

    Adăugat funcția loadTxt în fișierul OncicaVlad.py

---
+++ OncicaVlad.py
@@ -0,0 +1,8 @@
+def loadTxt(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+        return content
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
