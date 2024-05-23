ed874fa437b40fb11d2df9b404fd0632f2578b9c
unknown <mihaitg19@gmail.com>
2024-05-23 19:39:06 +0300

    tMihaiproblema

--- TeglerMihai.py
+++ TeglerMihai.py
@@ -4,9 +4,7 @@ def loadTxt(cale):
     with open(cale,'r') as file:
         reader=csv.reader(file)
         for row in reader:
-            print(row)
-    
-loadTxt('loadTxt.csv')
+            print(row) 
 
 def generateListFromString(x):
     list=[]
@@ -14,20 +12,12 @@ def generateListFromString(x):
         list.append(i)
     return list
 
-print(generateListFromString('Mihai are mere'))
-
 def uniques(y):
     n=set(y)
     return n
-
-list=[1,2,3,1,2,6,1,10,0,2,3,1,1,2,15,201,10,20,3]
-print(uniques(list))
-
 
 def listToTxt(y):
     with open('lista.txt','w') as file:
         unique_set=uniques(y)
         for item in unique_set:
             file.write(f'{item}\n')
-
-listToTxt(list)
